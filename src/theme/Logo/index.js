e0c33ae1e893d2e0a40644b8d68cab58f2e9f45b
juhnaorodrigues <juhnaorf@gmail.com>
2021-08-01 18:24:12 -0300

    teste remove import

--- src/theme/Logo/index.js
+++ src/theme/Logo/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Images from "../Logo/Images";
 import Image from 'next/image'
 import styled from "styled-components";
 
@@ -10,7 +9,7 @@ const DivImage = styled.div`
 export function Logo() {
     return (
         <DivImage>
-            <Image width='44px' height='35px' src="/instaluraIcon.png"/>
+            <Image width='44px' height='35px' lay src="/instaluraIcon.png"/>
             <Image width='96px' height='24px' src="/instaluraText.png"/>
         </DivImage>
     )
