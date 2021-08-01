9bda07d50a362f3af50a612e5e919ca00073da7d
juhnaorodrigues <juhnaorf@gmail.com>
2021-08-01 19:19:10 -0300

    teste image outra vez

--- src/theme/Logo/index.js
+++ src/theme/Logo/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from 'next/image'
 import styled from "styled-components";
-
+import profilePic from '/public/instalura_icon.png'
 const DivImage = styled.div` 
     display: flex;
 `;
@@ -9,7 +9,9 @@ const DivImage = styled.div`
 export function Logo() {
     return (
         <DivImage>
-            <Image width='144px' height='45px' src="/instalura_icon.png"/>
+            {/* <Image width='144px' height='45px' src="/instalura_icon.png"/> */}
+            <Image width='144px' height='45px' src={profilePic}/>
+            {/* <img width='144px' height='45px' src="instalura_icon.png"/> */}
         </DivImage>
     )
 }
