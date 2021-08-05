8390065eb4a0175125c2e62d132940f473a7a45a
juhnaorodrigues <juhnaorf@gmail.com>
2021-08-04 22:13:30 -0300

    reset, font, stilizacao e footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,23 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '../src/theme/GlobalStyle';
 import theme from '../src/theme';
+import Head from 'next/head'
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      <Head>
+        
+        <title>Instalura</title>
+        <link rel="icon" href="/aluraIcon.png" type="image/x-icon"/>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet"></link>
+
+      </Head>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
